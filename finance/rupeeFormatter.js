a2da5394773afe5e30fb09c690024950e1c86d82
Shubham Navale <shubham.navale@nitorinfotech.net>
2022-05-30 22:18:46 +0530

    Add USD currency support in rupee formatter

--- finance/rupeeFormatter.js
+++ finance/rupeeFormatter.js
@@ -1,10 +1,20 @@
 // FIXME: Add currency for other countries
-function rupeeFormatter(amount) {
+// Euro
+// USD
+// INR
+// POUND
+// YEN
+// AUS
+// CANDIAN Dollars
+function rupeeFormatter(amount, currency = 'INR') {
   // Error handling
   if (amount === undefined || amount === null) return amount;
 
   // Eg 19989899    899 = hundreds
   amount = amount.toString();
+  if (currency === 'USD') {
+    return amount.replace(/(?<=[0-9])(?=(?:[0-9]{3})+(?![0-9]))/g, ',');
+  }
   let hundreds = amount.substring(amount.length - 3);
   const remaining = amount.substring(0, amount.length - 3);
   if (remaining !== '') hundreds = ',' + hundreds;
